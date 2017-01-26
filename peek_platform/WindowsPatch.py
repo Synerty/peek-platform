a4dd472e5f4fa0c6ff6b1788994b56d96729b9cc
brentonford <brenton.ford@synerty.com>
2017-01-26 00:04:31 -0800

    windows create symlink patch
    ORMOB-69

--- peek_platform/WindowsPatch.py
+++ peek_platform/WindowsPatch.py
@@ -11,14 +11,17 @@ def createHardLink(src, dst):
     import ctypes
     flags = 1 if src is not None and os.path.isdir(src) else 0
     if not ctypes.windll.kernel32.CreateHardLinkA(dst, src, flags):
-        raise OSError
+        raise OSError(ctypes.get_last_error())
 
 
-def createSymbolicLink(src, dst):
-    import ctypes
-    flags = 1 if src is not None and os.path.isdir(src) else 0
-    if not ctypes.windll.kernel32.CreateSymbolicLinkA(dst, src, flags):
-        raise OSError
+def createSymbolicLink(src, dst, target_is_directory=False):
+    """ Create Symbolic Link
+
+    http://timgolden.me.uk/pywin32-docs/win32file__CreateSymbolicLink_meth.html
+    """
+    import win32file
+    flags = 1 if target_is_directory else 0
+    win32file.CreateSymbolicLink(dst, src, flags)
 
 
 if isWindows:
