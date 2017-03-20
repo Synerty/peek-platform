e89da87e12bc0115dd43ecafec0065e4e23a31ec
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-21 09:07:07 +1000

    Added support for renaming .mweb.html references to .ns.html

--- peek_platform/frontend/NativescriptBuilder.py
+++ peek_platform/frontend/NativescriptBuilder.py
@@ -190,9 +190,12 @@ class NativescriptBuilder(FrontendBuilderABC):
 
             elif inComponentHeader:
                 line = (line
-                        .replace(b'web.html', b'ns.html')
-                        .replace(b'web.css', b'ns.css')
-                        .replace(b'web.scss', b'ns.scss')
+                        .replace(b'.mweb.html', b'.ns.html')
+                        .replace(b'.mweb.css', b'.ns.css')
+                        .replace(b'.mweb.scss', b'.ns.scss')
+                        .replace(b'.web.html', b'.ns.html')
+                        .replace(b'.web.css', b'.ns.css')
+                        .replace(b'.web.scss', b'.ns.scss')
                         .replace(b"'./", b"'"))
 
             newContents += line
