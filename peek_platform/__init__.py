42b6b6993298216cf1259374815a43951e7570c8
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-04-12 23:59:12 +1000

    Updated to version 0.3.4.1

--- peek_platform/__init__.py
+++ peek_platform/__init__.py
@@ -1,6 +1,6 @@
 from . import WindowsPatch
 
-__version__ = '0.3.4'
+__version__ = '0.3.4.1'
 
 class PeekPlatformConfig:
     """ Peek Platform Config
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from peek_platform.WindowsPatch import isWindows
 
 pip_package_name = "peek-platform"
 py_package_name = "peek_platform"
-package_version = '0.3.4'
+package_version = '0.3.4.1'
 
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
