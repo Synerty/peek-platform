3c43ea596e6c9c4603b83969e7b01e12e20b67f3
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-21 17:01:46 +1000

    Fixed service log file name, fixed service restart patch
    ORMOB-600

--- peek_platform/util/LogUtil.py
+++ peek_platform/util/LogUtil.py
@@ -6,7 +6,7 @@ from txhttputil.util.LoggingUtil import LOG_FORMAT, DATE_FORMAT
 
 
 def setupServiceLogOutput(serviceName):
-    fileName = str(Path.home() / '%s.log' % serviceName)
+    fileName = str(Path.home() / ('%s.log' % serviceName))
 
     logFormatter = logging.Formatter(LOG_FORMAT, DATE_FORMAT)
     rootLogger = logging.getLogger()
