66a6d7b4205beae610b76e0af1b28acabe23c63a
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-09-05 17:05:22 +1000

    Updated to version 1.1.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from peek_platform.WindowsPatch import isWindows
 
 pip_package_name = "peek-platform"
 py_package_name = "peek_platform"
-package_version = '1.1.5'
+package_version = '1.1.6'
 
 egg_info = "%s.egg-info" % pip_package_name
 if os.path.isdir(egg_info):
