402e7195aa0e0651048dcb3eddae145c9d6eb475
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-12-05 02:24:39 +1000

    Pinned redis to before 3.0.0
    OR1812-54

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ requirements = [
     # Celery packages
     "flower",
     # "amqp >= 1.4.9",  # DEPENDENCY LINK BELOW
-    "redis >= 2.10.5",
+    "redis == 2.10.6",
 
     # Potentially useful packages
     # "GitPython >= 2.0.8",
