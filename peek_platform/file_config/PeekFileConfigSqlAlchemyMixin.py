5c570ca165f6c7546614dd9bdb331d25b3f66530
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-02-09 10:36:15 +1000

    Worker engine args are now supported in config.json
    PEEK-267

--- peek_platform/file_config/PeekFileConfigSqlAlchemyMixin.py
+++ peek_platform/file_config/PeekFileConfigSqlAlchemyMixin.py
@@ -1,8 +1,6 @@
 import logging
-from abc import ABCMeta
 
 from jsoncfg.value_mappers import require_string, require_dict
-from peek_platform.file_config.PeekFileConfigABC import PeekFileConfigABC
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +15,7 @@ class PeekFileConfigSqlAlchemyMixin:
     @property
     def dbEngineArgs(self):
         default = {
+            'echo': False,  # Print every SQL statement executed
             'pool_size': 20,  # Number of connections to keep open
             'max_overflow': 50,  # Number that the pool size can exceed when required
             'pool_timeout': 60,  # Timeout for getting conn from pool
