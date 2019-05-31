b74044cff3a975d0067f84523a40e1517ebfe11c
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-05-31 22:54:00 +1000

    Added support for faster DB insert

--- peek_platform/file_config/PeekFileConfigSqlAlchemyMixin.py
+++ peek_platform/file_config/PeekFileConfigSqlAlchemyMixin.py
@@ -19,7 +19,8 @@ class PeekFileConfigSqlAlchemyMixin:
             'pool_size': 20,  # Number of connections to keep open
             'max_overflow': 50,  # Number that the pool size can exceed when required
             'pool_timeout': 60,  # Timeout for getting conn from pool
-            'pool_recycle': 600  # Reconnect?? after 10 minutes
+            'pool_recycle': 600,  # Reconnect?? after 10 minutes
+            'use_batch_mode': True
         }
         with self._cfg as c:
             return c.sqlalchemy.engineArgs(default, require_dict)
