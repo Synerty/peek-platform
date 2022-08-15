54716f42f3c0dbcf51ed86c35a238883b67845ac
Jarrod Chesney <jarrod.chesney@synerty.com>
2022-08-15 18:26:51 +1000

    PEEK-1631 Py services now connect to peekServerHttpPort
    not peekServerVortexTcpPort

--- peek_platform/file_config/PeekFileConfigPeekServerClientMixin.py
+++ peek_platform/file_config/PeekFileConfigPeekServerClientMixin.py
@@ -15,11 +15,6 @@ class PeekFileConfigPeekServerClientMixin(metaclass=ABCMeta):
             return c.peekServer.httpPort(8011, require_integer)
 
     @property
-    def peekServerVortexTcpPort(self) -> int:
-        with self._cfg as c:
-            return c.peekServer.tcpVortexPort(8012, require_integer)
-
-    @property
     def peekServerHost(self) -> str:
         with self._cfg as c:
             return c.peekServer.host("127.0.0.1", require_string)
